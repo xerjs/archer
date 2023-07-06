0b5cc7874609036ff6b8447a859f1b2590e996e8
liuxing <lusionx@gmail.com>
2023-07-06 18:34:04 +0800

    fix(pkg): lint conf

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,8 @@ module.exports = {
     plugins: ["@typescript-eslint"],
     rules: {
         "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-var-requires": "off",
     },
 };
